946ab1041e8ced7b09f829481090cd5221e6769f
Dante Angel <dangel@terpmail.umd.edu>
2025-04-08 18:29:18 -0400

    Made a change to the practice_file

--- practice_file.py
+++ practice_file.py
@@ -10,4 +10,6 @@ print(f"Joined set: {set1|set2}")
 
 random_item = random.choice(list(set1|set2))
 print(f"Here is a random item from the the joined set: {random_item}")
+# random_item uses the import statement of random and choice method that is 
+# determined by a list with sets by using the union pipe operator.
  
