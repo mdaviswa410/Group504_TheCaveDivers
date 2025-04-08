8c1f59173ad1be4421267285139fe6791f3bc173
Maya Konduru <mjkond5@umd.edu>
2025-04-08 16:35:48 -0400

    used the random import statement and coded it to choose an item from the joined sets. I also added the print statement underneath.

--- practice_file.py
+++ practice_file.py
@@ -8,4 +8,6 @@ set1 = {"Beans", "eggs", "milk","gum"}
 set2 = {"gum", "steak","eggs" }
 print(f"Joined set: {set1|set2}")
 
+random_item = random.choice(list(set1|set2))
+print(f"Here is a random item from the the joined set: {random_item}")
  
