48e1cd11c32b59459494b02fdee0358e223b6c5b
Dante Angel <dangel@terpmail.umd.edu>
2025-04-18 09:43:01 -0400

    "Added the global variable"

--- interim_deliverable.py
+++ interim_deliverable.py
@@ -1,5 +1,9 @@
 import random
 
+CHALLENGES = {"Chal1" : 1, "Chal2" : 1, "Chal3" : 1, "Chal3" : 2, "Chal4" : 2, 
+              "Chal5" : 2, "Chal6" : 3, "Chal7" : 3, "Chal8" : 3, "Chal9" : 4, 
+              "Chal10" : 4, "Chal11" : 4, "Chal12" : 5, "Chal13" : 5, "Chal14" : 5}
+
 def create_battle(player_health, player_attack, player_defense,
                   enemy_health, enemy_attack, enemy_defense):
     round_num = 1
