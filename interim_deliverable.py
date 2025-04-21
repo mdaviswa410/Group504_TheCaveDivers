e43d0abf8e2e2f2ca2879c37ef6405d98ed1014a
Dante Angel <dangel@terpmail.umd.edu>
2025-04-21 12:37:21 -0400

    "Added comments and a docstring"

--- interim_deliverable.py
+++ interim_deliverable.py
@@ -1,11 +1,25 @@
-CHALLENGES = {"Chal1" : 1, "Chal2" : 1, "Chal3" : 1, "Chal3" : 2, "Chal4" : 2, 
-              "Chal5" : 2, "Chal6" : 3, "Chal7" : 3, "Chal8" : 3, "Chal9" : 4, 
-              "Chal10" : 4, "Chal11" : 4, "Chal12" : 5, "Chal13" : 5, "Chal14" : 5}
+# The variable CHALLENGES is a global variable for easy use anywhere.
+CHALLENGES = {"Chal1" : 1, "Chal2" : 1, "Chal3" : 1, "Chal4" : 2, "Chal5" : 2, 
+              "Chal6" : 2, "Chal7" : 3, "Chal8" : 3, "Chal9" : 3, "Chal10" : 4, 
+              "Chal11" : 4, "Chal12" : 4, "Chal13" : 5, "Chal14" : 5, "Chal15" : 5}
 
 
-def challenges():
+def challenges(hero_level):
+    """This will append challenges the hero can take
+    
+    Args:
+    hero_level (int): The level of the hero
+    
+    Returns:
+    available: A list of challenges when conditions are met the hero can do.
+    
+    Side effects:
+    The variable available is created as a list that has task appended.
+    
+    """
+    
     available = []
-    for task, level in CHALLENGES:
+    for task, level in CHALLENGES.items():
         if level == hero_level:
             available.append(task)
         
