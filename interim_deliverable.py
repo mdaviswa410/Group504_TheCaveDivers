a4205906a749f3801625c66189a2ed96684a30e1
Dante Angel <dangel@terpmail.umd.edu>
2025-04-21 09:40:31 -0400

    "Removed the random module"

--- interim_deliverable.py
+++ interim_deliverable.py
@@ -1,5 +1,3 @@
-import random
-
 CHALLENGES = {"Chal1" : 1, "Chal2" : 1, "Chal3" : 1, "Chal3" : 2, "Chal4" : 2, 
               "Chal5" : 2, "Chal6" : 3, "Chal7" : 3, "Chal8" : 3, "Chal9" : 4, 
               "Chal10" : 4, "Chal11" : 4, "Chal12" : 5, "Chal13" : 5, "Chal14" : 5}
