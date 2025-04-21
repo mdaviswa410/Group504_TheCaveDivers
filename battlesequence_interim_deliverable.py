9601ac3bb9d7de656ae206611a18cd7b69b29534
McKenzie Davis Watkins <mdaviswa@terpmail.umd.edu>
2025-04-21 09:37:08 -0400

    Updated with hashtags for myself, deciding range of ints for attacks.

--- battlesequence_interim_deliverable.py
+++ battlesequence_interim_deliverable.py
@@ -1,8 +1,10 @@
+#Function for the battle sequence algorithm focused on the attacks
 import random
-
+#Battle function with parameters for the player and the enemy.
 def create_battle(player_health, player_attack, player_defense,
                   enemy_health, enemy_attack, enemy_defense):
     round_num = 1
+#Damage taken to enemy by player and to player by enemy in between -5 and 5.
     while player_health > 0 and enemy_health > 0:
         return f"Round {round_num}"
         damage_enemy = max(0, player_attack - enemy_defense + 
@@ -16,7 +18,7 @@ def create_battle(player_health, player_attack, player_defense,
                         random.randint(-5, 5))
         player_health -= damage_player
         round_num += 1
-    
+#Battle results based on the health of the player and enemy.
     if player_health > 0 and enemy_health <= 0:
         battle_result = 'WIN'
     elif enemy_health > 0 and player_health <= 0:
