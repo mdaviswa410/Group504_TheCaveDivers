be8624a9f8f4b52afdedf57b46fa5a88b45d915b
Maya Konduru <mjkond5@umd.edu>
2025-04-21 12:17:33 -0400

    Add game.py file

---
+++ game.py
@@ -0,0 +1,112 @@
+"""
+    
+    This is a simple text-based rpg game where the user plays the role of the 
+    hero. As the adventure goes on the harder it gets.
+    
+    How it works:
+    The user will type in the number of the choice they want to pick and their 
+    actions will have specific consequences as the game progresses. 
+    
+    """
+def main():
+    """
+    Runs the main framework for our rpg game.
+    
+    Prompts the player for responses with choices
+    
+    Side Effects:
+        Uses the print statement to output things to the console
+        Uses input to change the choice made. 
+    """
+    
+
+    #Initilizing the game
+    
+    name = input("Enter your name hero: ")
+    inventory = []
+    location = "small_village"
+    health = 100
+    previous_choice = []
+    
+    scenes = {
+        "small_village":{
+            "desc": "You wake up in a small village, everything is peaceful."
+                    "The birds are chirping and there is a slight breeze." 
+                    "All of a sudden you here a knock at the door."
+                    "\n Please choose a number:", 
+            "choices": ["Open Door", "Stay in bed"]
+        },
+        "Open_Door":{
+            "desc": "You open the door and the town mayor approaches you. "
+                "He says he needs you help fighting off the monsters in time "
+                "for the village party.",
+            "choices": ["Accept", "Decline"]
+        },
+        "Forest_entrance":{
+            "desc": "The forest looks dark and dangerous.",
+            "choices": ["Enter Forest", "Go Back"]
+            
+        }
+            
+    }
+    
+           
+    game_over = False
+    print(f"Welcome {name}! Your adventure is about to begin!\n")
+    
+    while not game_over:
+        if health < 0:
+            print("Game over! You have perished")
+            break
+        
+        
+        #Setting the Scene
+        scene = scenes.get(location)
+        print(scene['desc'])
+        
+        
+        #Users choices
+        print("\nWhat would you like to do?")
+        for choice in range(len(scene['choices'])):
+            print(f"{choice + 1}. {scene['choices'][choice]}")
+            
+            
+        #Get Choice
+        try: 
+            choice_index = int(input("Choose an option: "))
+            if choice_index < 1 or choice_index > len(scene['choices']):
+                print("Invalid choice, please pick again!")
+                continue
+        except ValueError:
+            print("Please enter a number: ")
+            continue
+        
+        choice = scene['choices'][choice_index - 1]
+        previous_choice.append(choice)
+        print(f"\nYou have chosen: {choice}\n") 
+        
+        
+        if location == "small_village":
+            if choice == "Open Door":
+                location = "Open_Door"
+            elif choice == "Stay in bed":
+                print( "You get five more minutes of sleep but the knocking"               
+                "keeps going. You go to the doorway.")
+                location = "Open_door"
+    
+        elif location == "Open_Door":
+            if choice == "Accept":
+                location = "Forest_entrance"
+            elif choice == "Decline":
+                print("You refused to help. The mosters destroy your village "
+                        "Game over!")
+                game_over = True
+                
+        elif location == "Forest_entrance" :
+            if choice == "Enter Forest":
+                print("You bravely enter the forest...")
+            elif choice == "Go Back":
+                print("You return to the village. The mayor is quite upset.")
+                location == "small_village"
+if __name__ == "__main__":
+    main()
