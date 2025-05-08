71d017c18d2e69740082cfd33019ab7e3d46a7a2
McKenzie Davis Watkins <mdaviswa@terpmail.umd.edu>
2025-05-08 12:37:20 -0400

    added function that allows player to select a weapon for the battle, also changing the damage amount

--- battlesequence_interim_deliverable.py
+++ battlesequence_interim_deliverable.py
@@ -1,5 +1,49 @@
 #Function for the battle sequence algorithm focused on the attacks
 import random
+
+#Let player select weapon
+def select_weapon(inventory):
+    """_summary_
+
+    Args:
+        inventory (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    #Look in inventory and present weapons
+    weapon_keywords = ["Sword", "Axe", "Spear", "Flail"]
+    weapons = []
+    for item in inventory:
+        item_label = item[0]
+        for word in weapon_keywords:
+            if word in item_label:
+                weapons.append(item)
+                break
+    #Show inventory
+    print("Choose a weapon:")
+    for index in range(len(weapons)):
+        name = weapons[index][0]
+        value = weapons[index][1]
+        print(f"{name} | (Attack: {value})")
+    
+    #Let player input a number to select their weapon since its a tuple
+    while True:
+        try:
+            choice = int(input("Enter the number for your weapon:"))
+            if choice >=1 and choice <= len(weapons):
+                weapon_choice = weapons[choice - 1]
+                print(f"Your weapon is: {weapon_choice[0]}.")
+                return weapon_choice
+            else:
+                print("Invalid Number. Try again.")
+        
+        except ValueError:
+            print("Please enter a valid number.")
+        
+            
+                
+
 #Battle function with parameters for the player and the enemy.
 def create_battle(player_health, player_attack, player_defense,
                   enemy_health, enemy_attack, enemy_defense):
@@ -22,7 +66,7 @@ def create_battle(player_health, player_attack, player_defense,
     while player_health > 0 and enemy_health > 0:
         print(f"Round {round_num}")
         damage_enemy = max(0, player_attack - enemy_defense + 
-                           random.randint(-5, 5))
+                           random.randint(-3, 3))
         enemy_health -= damage_enemy
         
         print(f"Player hit Enemy for {damage_enemy} damage. \
@@ -32,8 +76,11 @@ def create_battle(player_health, player_attack, player_defense,
             break
     
         damage_player = max(0, enemy_attack - player_defense + 
-                        random.randint(-5, 5))
+                        random.randint(-3, 3))
         player_health -= damage_player
+        
+        if player_health <= 0:
+            break
         
         print(f"Enemy hit Player for {damage_player} damage. \
               Player health: {max(0, player_health)}")
@@ -47,4 +94,4 @@ def create_battle(player_health, player_attack, player_defense,
     else:
         battle_result = "DRAW"
     
-    return f"Battle Result: {battle_result.upper()}"
+    return f"Battle Result: {battle_result.upper()}"  
