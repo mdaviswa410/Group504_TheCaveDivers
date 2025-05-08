bf2d83d730e023c327ca4d7f579e3bbfdb6d3986
Dante Angel <dangel@terpmail.umd.edu>
2025-05-08 17:00:49 -0400

    "Attempted to finish the function"

--- interim_deliverable.py
+++ interim_deliverable.py
@@ -1,9 +1,10 @@
+import Loot_deliverable as potions
 # The variable CHALLENGES is a global variable for easy use anywhere.
 CHALLENGES = {"Chal1" : 1, "Chal2" : 1, "Chal3" : 1, "Chal4" : 2, "Chal5" : 2, 
               "Chal6" : 2, "Chal7" : 3, "Chal8" : 3, "Chal9" : 3, "Chal10" : 4, 
               "Chal11" : 4, "Chal12" : 4, "Chal13" : 5, "Chal14" : 5, "Chal15" : 5}
 
-
+'''
 def challenges(hero_level):
     """This will append challenges the hero can take
     
@@ -24,6 +25,39 @@ def challenges(hero_level):
             available.append(task)
         
     return available
+'''
+def get_potion_dict():
+    potion_dict = {}
+    all_potions = potions.items("")
+    print(all_potions)
+    for name, effect in all_potions:
+        if name in {"Health Potion", "Posion Potion", "Nothing"}:
+            potion_dict[name] = effect
+    return potion_dict
+    
+def heal_health(health, max_health, potion):
+    if health == max_health:
+        print("You have full health.")
+        return health
+    
+    print(f"\nYour health is {health}/{max_health}")
+    print("Potions to be used: ")
+    for name, healing in poitions.items():
+        print(f"{name}: +{healing} HP")
+        
+    choice = input("Do you want to use a potion?: Enter name or 'no': ").strip()
+    if choice in potions:
+        heal = potions[choice]
+        new_health = min(health + heal, max_health)
+        print(f"Your current health now is {new_health}/{max_health} ")
+        return new_health
+    else:
+        print("No potion used.")
+        return health
 
-def pass_or_fail(challenge):
-    pass
+if __name__ == "__main__":
+    health = 30
+    max_health = 50
+    potions = get_potion_dict()
+    health = heal_health(health, max_health, potions)
+        
