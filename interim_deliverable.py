617d232adf751927ef633f55c0c53f1184d79071
Dante Angel <dangel@terpmail.umd.edu>
2025-05-13 20:17:12 -0400

    "Made changes again that in the end useless"

--- interim_deliverable.py
+++ interim_deliverable.py
@@ -1,31 +1,71 @@
-from Loot_deliverable import find_loot
 import random
 
-challenges = {"forest_challenge" : False, "cave_challenge" : False
-              ,"dark_path_challenge" : False}
+medals = ["First Medal", "Second Medal", "Third Medal"]
+medals_obtained = []
 
-def run_challenge(name_challenge, inventory):
-    if challenges[name_challenge]:
+def challenge_status(name_challenge, attempted = None):
+    attempted = attempted or set()
+    allowed_challenges = {"forest_challenge", "cave_challenge"
+              ,"dark_path_challenge"}
+    return (False if (name_challenge in (attempted & allowed_challenges))
+            else True)
+
+def run_challenges(scene_key, medals, inventory, attempted):
+    names = {"Forest_entrance" : "forest_challenge", 
+             "Cave_or_Mountain" : "cave_challenge",
+             "The_Dark_Path" : "dark_path_challenge"}
+    
+    type_challenge = {"forest_challenge" : "coin", "cave_challenge" : "number",
+                      "dark_path_challenge" : "dice"}
+    
+    names_challenges = names.get(scene_key)
+    if not names_challenges:
+        print("No challenge")
         return inventory
     
-    print(f"Challenge: {name_challenge.replace("_", " ").title()}")
-    success = random.choice([True, False])
+    if not challenge_status(names_challenges, attempted):
+        print("You already attempted this")
+        return inventory
     
-    if success:
-        print("You passed the challenge, congrats")
-        reward = find_loot()
-        inventory.append(reward)
-        print(f"You got {reward}")
-    else:
-        print("You failed the challenge.")
+    print(f"\n {names_challenges.replace("_", " ").title()}")
+    attempted.add(names_challenges)
+    kind = type_challenge[names_challenges]
+    success = False
     
-    challenges[name_challenge] = True
-    return inventory
+    if kind == "coin":
+        guess = input("Guess heads or tails: ").strip().lower()
+        result = random.choice(["heads", "tails"])
+        print(f"The coin lands on {result}")
+        success = (guess == result)
+    
+    elif kind == "number":
+        try:
+            guess = int(input("Guess a number between 1 to 4: "))
+            actual_num = random.randint(1, 4)
+            print(f"The number is {actual_num}")
+            success = (guess == actual_num)
+        except ValueError:
+            print("Invalid number")
+    
+    elif kind == "dice":
+        try:
+            guess = int(input("Guess a dice roll between 1 to 6: "))
+            actual_roll = random.randint(1, 6)
+            print(f"Dice roll is {actual_roll}")
+            success = (guess == actual_roll)
+        except ValueError:
+            print("Invalid number")
 
-def check_challenges(scene_key):
-    return {"Forest_entrance" : "forest_challenge"
-            , "Cave_or_Mountain" : "cave_challenge"
-             , "The_Dark_Path" : "dark_path_challenge"}.get(scene_key, None)
+    if success:
+        if medals:
+            reward = medals.pop(0)
+            inventory.append(reward)
+            print(f"You won {reward}")
+        else:
+            print("You have completed challenge, but there is no medals left")
+    else:
+        print("You lost this challenge")
+    return inventory
 
 def challenge_status(name_challenge, attempted = None):
     attempted = attempted or set()
@@ -59,6 +99,6 @@ def heal_health(health, max_health, potions):
 
 if __name__ == "__main__":
     health = 30
-    max_health = 100
+    max_health = 50
     potions = get_potion_dict()
     health = heal_health(health, max_health, potions)
